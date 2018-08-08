74752ab7e808bad77c41c9d7e9eef99d8ed028ca
Knut Melvær <knut.melvaer@gmail.com>
2018-08-08 22:03:37 +0200

    fix localport and proxy local api to sanity

--- server.js
+++ server.js
@@ -1,23 +1,32 @@
 const express = require('express');
 const next = require('next');
+const sanityClient = require('@sanity/client')
 const Router = require('./routes').Router;
-
 const dev = process.env.NODE_ENV !== 'production';
-const port = parseInt(process.env.PORT, 10) || 6969;
+const port = parseInt(process.env.PORT, 10) || 3000;
 const app = next({ dev });
 const handle = app.getRequestHandler();
-const { getShows, getShow } = require('./lib/getShows');
+
+const client = sanityClient({
+  projectId: 'zx5z31i3',
+  dataset: 'production',
+  useCdn: true
+})
+
+function getShow({res, from = 0, to = 100}) {
+  return client.fetch(`*[_type == "episode"][${from}..${to}]`).then(shows => res.json(shows)).catch(err => res.json({ err }));
+}
 
 app.prepare().then(() => {
   const server = express();
 
   // API endpoints
   server.get('/api/shows', (req, res) => {
-    res.json(getShows());
+    return getShow({res})
   });
 
-  server.get('/api/shows/:number', (req, res) => {
-    const show = getShow(req.params.number);
+  server.get('/api/shows/:number', async ({params: { number = 0 } = {}}, res) => {
+    const show = await getShow({res, from: number, to: number });
     if (show) {
       res.json(show);
       return;
