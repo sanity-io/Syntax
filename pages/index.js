90e214202cd40fcd2a86ec0c132008d8d2f54110
Knut Melvær <knut.melvaer@gmail.com>
2018-08-08 17:32:21 +0200

    replace datafetching with Sanity

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import axios from 'axios';
 import ShowList from '../components/ShowList';
 import ShowNotes from '../components/ShowNotes';
 import Player from '../components/Player';
@@ -7,6 +6,15 @@ import Meta from '../components/meta';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Page from '../components/Page';
+import client from '../lib/client';
+
+const query = `{
+  "shows": *[_type == "episode"]|order(schedule.publish desc),
+  "podcast": *[_type == "podcast" && slug.current == $slug][0]{
+    ...,
+    "hosts": hosts[]->
+  }
+}`
 
 export default class IndexPage extends React.Component {
   constructor(props) {
@@ -22,8 +30,9 @@ export default class IndexPage extends React.Component {
   static async getInitialProps({ req }) {
     const protocol = req && req.headers.host.indexOf('syntax.fm') > -1 ? 'https' : req ? req.protocol : '';
     const baseURL = req ? `${protocol}://${req.headers.host}` : window.location.origin;
-    const { data: shows } = await axios.get(`${baseURL}/api/shows`);
-    return { shows, baseURL };
+    const { shows, podcast } = await client.fetch(query, { slug: "syntax"})
+    console.log(podcast)
+    return { shows, podcast, baseURL };
   }
 
   componentWillReceiveProps(nextProps) {
@@ -39,14 +48,14 @@ export default class IndexPage extends React.Component {
   };
 
   render() {
-    const { shows = [], baseURL } = this.props;
+    const { shows = [], podcast, baseURL } = this.props;
     const { currentShow, currentPlaying } = this.state;
     // Currently Shown shownotes
     const show = shows.find(show => show.displayNumber === currentShow);
     // Currently Playing
     const current = shows.find(show => show.displayNumber === currentPlaying);
     return (
-      <Page>
+      <Page podcast={podcast} >
         <Meta show={show} baseURL={baseURL} />
         <div className="wrapper">
           <div className="show-wrap">
