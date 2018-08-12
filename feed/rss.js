a3ba689c30777274cd71f70ce979bf8f5ea8fddf
Knut Melvær <knut.melvaer@gmail.com>
2018-08-12 12:09:43 +0200

    move babelrc to functions folder

--- feed/rss.js
+++ feed/rss.js
@@ -14935,7 +14935,47 @@ module.exports = function (h) {
 "use strict";
 
 
-module.exports = "*[_type == \"podcast\" && slug.current == \"syntax\"][0]{\n    title,\n    subtitle,\n    \"site_url\": itunes.url,\n    \"feed_url\": \"https://syntax.fm/feed\",\n    language,\n    copyright,\n    language,\n    \"itunesSubtitle\": subtitle,\n    \"itunesAuthor\": itunes.author,\n    \"itunesSummary\": itunes.summary,\n    description,\n    \"itunesOwner\": {\n      \"itunesName\": itunes.owner.name,\n      \"itunesEmail\": itunes.owner.email\n    },\n    \"itunesType\": itunes.type,\n    \"itunesImage\": coverArt.asset->url,\n    \"slug\": slug.current,\n    \"itunesCategories\": {\n      \"primary\": itunes.categories.firstCategory,\n      \"secondary\": itunes.categories.secondaryCategory,\n      \"tertiary\": itunes.categories.tertiaryCategory\n    },\n    \"episodes\": *[references(^._id) && _type == \"episode\"]{\n      title,\n      description,\n      \"slug\": slug.current,\n      \"guid\": _id,\n      \"pubDate\": schedule.publish,\n      \"enclosureUrl\": file.asset->url,\n      \"enclosureLength\": file.asset->size,\n      \"enclosureType\": file.asset->mimeType,\n      \"enclosureSize\": file.asset->size,\n      \"itunesEpisodeType\": itunes.type,\n      \"itunesDuration\": duration,\n      \"itunesExplicit\": explicit,\n      \"itunesImageHref\": coverArt.asset->url,\n      \"itunesSummary\": summary\n    }\n}";
+module.exports = `*[_type == "podcast" && slug.current == "syntax"][0]{
+    title,
+    subtitle,
+    "site_url": itunes.url,
+    "feed_url": "https://syntax.fm/feed",
+    language,
+    copyright,
+    language,
+    "itunesSubtitle": subtitle,
+    "itunesAuthor": itunes.author,
+    "itunesSummary": itunes.summary,
+    description,
+    "itunesOwner": {
+      "itunesName": itunes.owner.name,
+      "itunesEmail": itunes.owner.email
+    },
+    "itunesType": itunes.type,
+    "itunesImage": coverArt.asset->url,
+    "slug": slug.current,
+    "itunesCategories": {
+      "primary": itunes.categories.firstCategory,
+      "secondary": itunes.categories.secondaryCategory,
+      "tertiary": itunes.categories.tertiaryCategory
+    },
+    "episodes": *[references(^._id) && _type == "episode"]{
+      title,
+      description,
+      "slug": slug.current,
+      "guid": _id,
+      "pubDate": schedule.publish,
+      "enclosureUrl": file.asset->url,
+      "enclosureLength": file.asset->size,
+      "enclosureType": file.asset->mimeType,
+      "enclosureSize": file.asset->size,
+      "itunesEpisodeType": itunes.type,
+      "itunesDuration": duration,
+      "itunesExplicit": explicit,
+      "itunesImageHref": coverArt.asset->url,
+      "itunesSummary": summary
+    }
+}`;
 
 /***/ })
 /******/ ])));
