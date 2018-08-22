1895e85c299710ab364f2864ec3c0d5d36cef0f5
Knut Melvær <knut.melvaer@gmail.com>
2018-08-22 17:57:05 +0200

    add routing

--- next.config.js
+++ next.config.js
@@ -1,9 +1,33 @@
 const path = require('path');
 const glob = require('glob');
+const client = require('./lib/client')
+const slug = require('speakingurl')
+const query = `{
+  "shows": *[_type == "episode" && (defined(file) || defined(fileUrl))]{
+    ...,
+    "url": coalesce(fileUrl, file.asset->url)
+    }|order(schedule.publish desc),
+  "podcast": *[_type == "podcast" && slug.current == $slug][0]{
+    ...,
+    "hosts": hosts[]->
+  }|order(_publishedAt asc)
+}`
+
+const params = { slug: "syntax"}
+
+async function getData() {
+  const { shows = [], podcast } = await client.fetch(query, params)
+  const showsNumber = shows.reduce((acc, curr, index) => ({
+    ...acc,
+    [`/show/0${shows.length - index}/${slug(curr.title)}`]: { page: '/', query: { build: true }}}), {})
+  return { shows: showsNumber }
+}
 
 module.exports = {
   exportPathMap: async function (defaultPathMap) {
+    const {shows} = await getData()
     return {
+      ...shows,
       '/': { page: '/', query: { build: true } },
       '/sponsor': { page: 'sponsor', query: { build: true } }
     }
